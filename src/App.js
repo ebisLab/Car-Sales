78444b1d3e0fbbdc1a81fdafba8fcbd789100c5c
ebisLab <ebaiden@techie.com>
2019-08-15 05:46:38 -0400

    met MVP

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { createStore } from 'redux';
-// import {Provider} from 'react-redux';
 import { connect } from 'react-redux';
 
 import Header from './components/Header';
@@ -9,26 +8,9 @@ import AdditionalFeatures from './components/AdditionalFeatures';
 import Total from './components/Total';
 import {toggleAddBtn} from './actions'
 
-// import { Reducer } from './reducer'
-
-
-
-// const store = createStore(Reducer);
-// console.log(store)
-
 const App = () => {
-  // console.log(props, 'App prps')
-
-  const removeFeature = item => {
-    // dispatch an action here to remove an item
-  };
-
-  const buyItem = item => {
-    // dipsatch an action here to add an item
-  };
 
   return (
-    // <Provider store={store}>   {/* define store into provider */}
     <div className="boxes">
       <div className="box">
          <Header/>
@@ -39,17 +21,8 @@ const App = () => {
         <Total/>
       </div>
     </div>
-    // </Provider>
   );
 };
 
-// const mapStateToProps = state => {
-//   console.log(state, 'state');
-//   return {
-//     car: state.car,
-//     store: state.store,
-//     additionalPrice: state.additionalPrice
-//   };
-// }
 
-export default  /*connect(mapStateToProps, {toggleAddBtn})*/(App);
+export default  (App);
